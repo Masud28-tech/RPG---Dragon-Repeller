13fd277e532dce7fb09e7e4429c6a5f8320be973
Masud Ansari <65458743+Masud28-tech@users.noreply.github.com>
2024-04-10 00:12:05 +0530

    work done till step 106

--- script.js
+++ script.js
@@ -134,5 +134,8 @@ function sellWeapon() {
   if(inventory.length > 1){
     gold += 15;
     goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
   }
 }
