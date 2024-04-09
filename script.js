cae31eae4dd9f3dc006d18acfb459fd601fed7ab
Masud Ansari <65458743+Masud28-tech@users.noreply.github.com>
2024-04-10 00:05:15 +0530

    work done till step 102

--- script.js
+++ script.js
@@ -105,9 +105,24 @@ function buyHealth() {
   }
 }
 
-function buyWeapon() { 
-  if(gold >= 30){
-    gold -= 30;
+function buyWeapon() {
+  if (currentWeapon < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    }
+    else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
   }
 }
 
@@ -115,3 +130,9 @@ function fightSlime() { }
 
 function fightBeast() { }
 
+function sellWeapon() { 
+  if(inventory.length > 1){
+    gold += 15;
+    goldText.innerText = gold;
+  }
+}
